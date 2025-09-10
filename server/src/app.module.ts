b510fb03396133832e21588bb2193f6d7c6accb9
Phoomss <narongsak5az@gmail.com>
2025-09-10 12:54:20 +0700

    set connectting database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,18 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { ProductModule } from './order/product/product.module';
-import { ProductModule } from './payment/product/product.module';
 import { PaymentModule } from './payment/payment.module';
 import { OrderModule } from './order/order.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AddressOptionModule } from './address_option/address_option.module';
 import { AddressModule } from './address/address.module';
-import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, PaymentModule, OrderModule, AddressModule, AddressOptionModule, UserModule, AuthModule],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'somaricha',
+      // entities: [Product,Category],
+      synchronize: true,
+    }),
+    ProductModule,
+    PaymentModule,
+    OrderModule,
+    AddressModule,
+    AddressOptionModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
