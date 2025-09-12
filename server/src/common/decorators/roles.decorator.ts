7ee7f6352d285ccbd6faea2252d651be7783a01f
Phoomss <narongsak5az@gmail.com>
2025-09-13 00:20:07 +0700

    add guard

---
+++ server/src/common/decorators/roles.decorator.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { UserRole } from 'src/user/dto/create-user.dto';
+import { ROLES_KEY } from '../guards/roles.guard';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRoles.includes(user?.user_role);
+  }
+}
