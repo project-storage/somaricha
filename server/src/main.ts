3150c7f6339c741bb17fb9acfd2e1d440acca27a
Phoomss <narongsak5az@gmail.com>
2025-09-14 20:10:45 +0700

    set cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true, // if have send cookie/token
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   const config = new DocumentBuilder()
     .setTitle('Somaricha API')
     .setDescription('API for development product somaricha')
@@ -13,7 +19,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
