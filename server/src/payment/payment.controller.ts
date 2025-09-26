0d76d15907fac4da75bbbe9e672dc814b335eb62
Phoomss <narongsak5az@gmail.com>
2025-09-26 14:01:11 +0700

    change swagger

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -6,27 +6,19 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { UserRole } from 'src/user/dto/create-user.dto';
-import { ApiBearerAuth, ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@ApiTags('payments')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token') // ใช้ JWT token
-  @ApiOperation({ summary: 'Create a payment (Admin only)' })
-  @ApiResponse({ status: 201, description: 'Payment created successfully.' })
   @Post()
   create(@Body() dto: CreatePaymentDto) {
     return this.paymentService.create(dto);
   }
 
   @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Get all payments' })
-  @ApiResponse({ status: 200, description: 'List of payments.' })
   @Get()
   findAll() {
     return this.paymentService.findAll();
@@ -34,8 +26,6 @@ export class PaymentController {
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Update a payment (Admin only)' })
   @Patch(':id')
   update(@Param('id') id: number, @Body() dto: UpdatePaymentDto) {
     return this.paymentService.update(id, dto);
@@ -43,8 +33,6 @@ export class PaymentController {
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Delete a payment (Admin only)' })
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.paymentService.remove(id);
--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -15,32 +15,23 @@ import { UserRole } from 'src/user/dto/create-user.dto';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiTags, ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@ApiTags('products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Create product (Owner only)' })
-  @ApiResponse({ status: 201, description: 'Product created successfully.' })
   @Post()
   create(@Body() dto: CreateProductDto) {
     return this.productService.create(dto);
   }
 
-  @ApiOperation({ summary: 'Get all products' })
-  @ApiResponse({ status: 200, description: 'List of products.' })
   @Get()
   findAll() {
     return this.productService.findAll();
   }
 
-  @ApiOperation({ summary: 'Get product by ID' })
-  @ApiResponse({ status: 200, description: 'Product detail.' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
@@ -48,8 +39,6 @@ export class ProductController {
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Update product (Owner only)' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
     return this.productService.update(+id, dto);
@@ -57,8 +46,6 @@ export class ProductController {
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.OWER)
-  @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Delete product (Owner only)' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
