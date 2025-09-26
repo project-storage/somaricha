147d8ab9685657717efe7688a440355458ec4fcf
Phoomss <narongsak5az@gmail.com>
2025-09-26 14:25:35 +0700

    address and address option

--- server/src/address/address.module.ts
+++ server/src/address/address.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressOption } from 'src/address_option/entities/address_option.entity';
+import { Address } from './entities/address.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Address, AddressOption])],
   controllers: [AddressController],
   providers: [AddressService],
+  exports: [AddressService],
 })
 export class AddressModule {}
--- server/src/address_option/address_option.module.ts
+++ server/src/address_option/address_option.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AddressOptionService } from './address_option.service';
 import { AddressOptionController } from './address_option.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressOption } from './entities/address_option.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AddressOption])],
   controllers: [AddressOptionController],
   providers: [AddressOptionService],
+  exports: [AddressOptionService],
 })
 export class AddressOptionModule {}
