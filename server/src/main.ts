c786e19cfb148130990ee3df551d221bea7f1629
Phoomss <narongsak5az@gmail.com>
2025-10-20 12:50:38 +0700

    fix: update CORS origin to production URL

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://somaricha.vercel.app/',
     credentials: true, // if have send cookie/token
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
